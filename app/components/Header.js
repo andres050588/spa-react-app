d5525259c401ad3ab4d986a5a1d6901f3eaec660
andres050588 <andres050588@gmail.com>
2023-01-04 23:42:38 +0100

    Added About and Terms pages and installed npm react-router-dom for browsing pages with BrowserRouter, Routes, Route with their 'about-us' and 'terms' paths, import also Link from react-router-dom for all <a> tags and added historyApiFallback: { index: index.html } in webpack.config file, created Container tag that contain Page Tag with arg wide and title

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,14 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <>
-      <header className="header-bar bg-primary mb-3">
+      <header className="header-bar bg-success mb-3">
         <div className="container d-flex flex-column flex-md-row align-items-center p-3">
           <h4 className="my-0 mr-md-auto font-weight-normal">
-            <a href="/" className="text-white">
+            <Link to="/" className="text-white">
               ComplexApp
-            </a>
+            </Link>
           </h4>
           <form className="mb-0 pt-2 pt-md-0">
             <div className="row align-items-center">
@@ -19,7 +20,7 @@ function Header() {
                 <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
               </div>
               <div className="col-md-auto">
-                <button className="btn btn-success btn-sm">Sign In</button>
+                <button className="btn btn-outline-light btn-sm">Sign In</button>
               </div>
             </div>
           </form>
